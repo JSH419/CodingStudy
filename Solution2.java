92d48bde331dc73e1a31ae09a3daf43a9f20cfc3
JSH419 <122865395+JSH419@users.noreply.github.com>
2023-03-23 19:07:54 +0900

    Add files via upload
    switch문 연습입니다.

--- Solution2.java
+++ Solution2.java
@@ -1,29 +1,99 @@
-package 스터디예제9;
-/*
- * 
- * import java.util.Arrays;
+package 스터디예제11;
 
-	class Solution {
-    public int solution(int[] array, int n) {
-        return (int) Arrays.stream(array)  // 배열을 스트림으로 변환
-        .filter(num -> num == n)  // n과 같은 원소만 걸러냄
-        .count();  // 걸러낸 원소의 개수를 반환
-    }
-}
- * 스트림으로 풀 수 있다. 
- */
+class Solution2 {
+	    public String solution(String letter) {		//모스부호를 letter로 받아서 영어로 변환한다. 
+	        String answer = "";		
+	        String [] morse = {".-","-...","-.-.","-..",".","..-.","--.","....","..",".---",
+	        				  "-.-",".-..","--","-.","---",".--.","--.-",".-.","...","-","..-","...-",
+	        				  ".--","-..-","-.--","--.."};
+	        String [] req = letter.split(" ");	// letter 문자열을 공백으로 분리한다. 분리된 문자열을 배열의 요소에 담는다. ex: "hello world"를 hello와 world로 나눠서  -> 배열 요소 0, 배열 요소 1에 따로 넣어줌. 
+	        String word = "";	
+	        
+	        for(int j=0; j<req.length; j++){	
+	            switch(req[j]){							// req[j]와 일치하는 case문으로 이동한다. 
+	                case ".-" : 
+	                    word += req[j].replace(".-", "a");
+	                    break;
+	                case "-..." : 
+	                    word += req[j].replace("-...", "b");
+	                    break;
+	                case "-.-." : 
+	                    word += req[j].replace("-.-.", "c");
+	                    break;
+	                case "-.." : 
+	                    word += req[j].replace("-..", "d");
+	                    break;
+	                case "." :
+	                    word += req[j].replace(".", "e");
+	                    break;
+	                case "..-." : 
+	                    word += req[j].replace("..-.", "f");
+	                    break;
+	                case "--." : 
+	                    word += req[j].replace("--.", "g");
+	                    break;
+	                case "...." : 
+	                    word += req[j].replace("....", "h");
+	                    break;
+	                case ".." : 
+	                    word += req[j].replace("..", "i");
+	                    break;
+	                case ".---" : 
+	                    word += req[j].replace(".---", "j");
+	                    break;
+	                case "-.-" : 
+	                    word += req[j].replace("-.-", "k");
+	                    break;
+	                case ".-.." :
+	                    word += req[j].replace(".-..", "l");
+	                    break;
+	                case "--" : 
+	                    word += req[j].replace("--", "m");
+	                    break;
+	                case "-." : 
+	                    word += req[j].replace("-.", "n");
+	                    break;
+	                case "---" :
+	                    word += req[j].replace("---", "o");
+	                    break;
+	                case ".--." : 
+	                    word += req[j].replace(".--.", "p");
+	                    break;
+	                case "--.-" : 
+	                    word += req[j].replace("--.-", "q");
+	                    break;
+	                case ".-." : 
+	                    word += req[j].replace(".-.", "r");
+	                    break;
+	                case "..." : 
+	                    word += req[j].replace("...", "s");
+	                    break;
+	                case "-" : 
+	                    word += req[j].replace("-", "t");
+	                    break;
+	                case "..-" : 
+	                    word += req[j].replace("..-", "u");
+	                    break;
+	                case "...-" : 
+	                    word += req[j].replace("...-", "v");
+	                    break;
+	                case ".--" : 
+	                    word += req[j].replace(".--", "w");
+	                    break;
+	                case "-..-" : 
+	                    word += req[j].replace("-..-", "x");
+	                    break;
+	                case "-.--" : 
+	                    word += req[j].replace("-.--", "y");
+	                    break;
+	                case "--.." : 
+	                    word += req[j].replace("--..", "z");
+	                    break;
+	            }
 
+	            answer = word;
+	        }
 
-public class Solution2 {
-	public int solution(int[] array, int n) {
-        int answer = 0;
-        
-        for(int num : array) {
-        	if(num == n) {
-        		answer++;
-        	}
-        }
-        return answer;
-    }
-}
-
+	        return answer;
+	    }
+	}
