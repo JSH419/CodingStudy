6c4a4ce092551b75c07c532d20dfb19d2e5a4eb6
JSH419 <122865395+JSH419@users.noreply.github.com>
2023-03-26 23:57:47 +0900

    Add files via upload
    printf를 이용한 출력 예제. 
    뭔가 복잡해 보이고 이해가 잘 되지 않음. 다시 읽어보기 위해 코드를 따라 쳐봄.

---
+++ Ex2_9.java
@@ -0,0 +1,19 @@
+class Ex2_9 {
+	public static void main(String[] args) {
+		String url = "www.codechobo.com";
+		float f1 = .10f;
+		float f2 = 121f;
+		float f3 = 3.14e3f;
+		double d = 1.23457979;
+		System.out.printf("f1=%f, %e, %g%n", f1, f1, f1);
+		System.out.printf("f2=%f, %e, %g%n", f2, f2, f2);
+		System.out.printf("f3=%f, %e, %g%n", f3, f3, f3);
+		System.out.printf("d=%f%n", d);
+		System.out.printf("d=%14.10f%n", d);
+		System.out.printf("[12345678901234567890]%n");
+		System.out.printf("[%s]%n", url);
+		System.out.printf("[%20s]%n", url);
+		System.out.printf("[%-20s]%n", url);
+		System.out.printf("[%.8s]%n", url);
+	}
+}
