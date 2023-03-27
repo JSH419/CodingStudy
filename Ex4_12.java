da18495c84a33c6ea991b2370ee4e1b540449293
Dio <122865395+JSH419@users.noreply.github.com>
2023-03-27 19:15:55 +0900

    Add files via upload
    i can do it? i can do it!

---
+++ Ex4_12.java
@@ -0,0 +1,11 @@
+
+public class Ex4_12 {
+	public static void main(String[] args) {
+		int i = 5; 
+		
+		while(i--!=0) {
+			System.out.println(i + " - I can do it. ");
+		}
+	}
+}
+
---
+++ Ex4_13.java
@@ -0,0 +1,12 @@
+
+public class Ex4_13 {
+	public static void main(String[] args) {
+		int sum = 0;
+		int i = 0;
+		
+		while(sum <= 100) {
+			System.out.printf("%d - %d%n", i, sum);
+			sum += ++i;
+		}
+	}
+}
